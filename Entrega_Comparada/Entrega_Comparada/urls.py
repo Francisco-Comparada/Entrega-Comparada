e303bdbdf98178a0b3ecc9b3281d2e53e1aa735e
Francico Comparada <francisco.comparada@gmail.com>
2022-08-08 15:41:13 -0300

    index.html

--- Entrega_Comparada/Entrega_Comparada/urls.py
+++ Entrega_Comparada/Entrega_Comparada/urls.py
@@ -1,10 +1,12 @@
+from operator import index
 from django.contrib import admin
 from django.urls import path,include
+from Entrega_Comparada.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
-     path('sneakers/', include('sneakers.urls'))
+    path('',index),
+    
+    path('sneakers/', include('sneakers.urls'))
 ]
